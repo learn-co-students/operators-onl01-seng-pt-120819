bd79bb00e2c71ad8c360871b2c5f91a8d796d61e
Alexis Priore <alexispriore@gmail.com>
2019-12-13 18:35:27 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,25 @@
 def unsafe?(speed)
+  
+  if unsafe (speed > 60)
 
 end
 
 
 
 def not_safe?(speed)
+  
+  if (speed > 40)
 	
 end
 	
+	
+	
+	def unsafe?(speed)
+	  
+ if (speed 40 == 60)
 
+end
 
+def not_safe(speed)
+  
+  if (speed < 60)
