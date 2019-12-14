ed3d6bc5e733d6f4c472d3c46f7b3e4c18fe3e88
Asif Kamal <asif_kamal@hotmail.com>
2019-12-14 18:45:07 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,15 @@
 def unsafe?(speed)
-
+  if 40 > speed || 60 < speed
+    return true
+  else
+    return false
+  end
 end
 
 
 
 def not_safe?(speed)
-	
+	40 > speed || 60 < speed ? true : false 
 end
 	
 
